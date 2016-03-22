986eab707d86cd77505234a9225059176c33074d
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2016-03-22 14:40:54 -0700

    Numerical versions of some of the functions

--- astrochairs/graphscheduler.py
+++ astrochairs/graphscheduler.py
@@ -130,6 +130,72 @@ def add_edges(G, labels=None, hard_constraint=True, min_importance=0.1, max_impo
     return G
 
 
+def add_edges_numerical(G, edges, hard_constraint=True, threshold=0.5):
+    """
+    Add edges to the graph, with weights. 
+    Weights are determined by by the importance weights on 
+    each label.
+
+    If no order of labels is 
+    specified, then the order of keys in the dictionary 
+    for each node will be used.
+    
+    Parameters
+    ----------
+    G : networkx.Graph() instance
+        The graph without edges
+ 
+    hard_constraint : bool
+        Boolean flag determining whether hard constraints should be used. 
+        In this case, this means that for the first label specified in 
+        `labels`, no edges will be drawn when this label is the same 
+        for two nodes. 
+
+    threshold: float out of [0,1]
+        The threshold of the dissimilarity measure describing how dissimilar 
+        two sessions are. If the threshold is small, the graph has a high tolerance 
+        to similar sessions. That is, it is more likely to schedule similar sessions 
+        at the same time. If it is small, then this tolerance is much smaller. There 
+        will be much fewer edges between nodes. This means that there is a much 
+        smaller chance that similar sessions will be scheduled at the same time. 
+        However, fewer edges also means that there's a greater chance that no 
+        solution will be found at all.
+        
+        
+    Returns
+    -------
+    G : networkx.Graph() instance
+        The same input graph, but with edges.
+    """
+    
+    n_nodes = G.number_of_nodes()
+
+    assert np.size(edges) == (n_nodes*(n_nodes - 1))/2.0, "Incorrect number of edges!"
+    
+    # counter for edges
+    i = 0
+    
+    for k, n1 in enumerate(G.nodes()):
+        for l, n2 in enumerate(G.nodes()):
+            if k <= l:
+                continue
+            # if a hard constraint is set, make no edge 
+            # if the edge weight is smaller than `threshold`,
+            # otherwise set the weight edge to 1
+            if hard_constraint:
+                if edges[i] < threshold:
+                    i += 1
+                    continue
+                else:
+                    G.add_edge(n1, n2, weight=1.0)
+            # otherwise just set all the weights the way they are
+            else:
+                G.add_edge(n1,n2,weight=edges[i])
+            
+            i += 1
+            
+    return G
+
 def _sort_cliques_by_weights(G, cliques, n_elements):
     """
     Sort cliques by their weights.
@@ -326,6 +392,136 @@ def find_solution(G, n_elements, n_unused=None, results=None):
 
         results.success = False
         return results
+
+def find_solution_numerical(G, n_elements, n_unused=None, results=None):
+    """
+    Sort nodes in G into groups of n_elements members such that 
+    the total sum of weights is maximized. 
+    If the graph includes hard constraints on the relationship between 
+    nodes (i.e. missing edges), it is possible that no solution is found.
+    
+    In the case of a fully connected graph, the solution will be that 
+    which maximizes the weights. The weights are inherent attributes of 
+    the Graph and must be calculated beforehand (see `add_edges` for details).
+    
+    Parameters
+    ----------
+    G : networkx.Graph() instance
+        Undirected graph with nodes and edges. The edges must have weights 
+        between 0 and 1, but edges can be missing if no relationship exists 
+        between nodes.
+                
+    n_elements : integer
+        The number of elements per group. Must be an integer divisor of the 
+        total number of nodes in the graph.
+        
+    n_unused : integer
+        The number of unused nodes in the graph at every recursion step. 
+        If None, then it will be initialized as the total number of nodes
+        in the graph.
+    
+    weights_total_sum : list
+        The total sum of weights of elements in `groups`. 
+        If None, then it will be initialized as an empty list to count 
+        the sum of weights for each individual group. Will be summed at 
+        the end before output into a float value.
+        Note: DO NOT SET THIS AT THE BEGINNING OF THE RUN!
+        
+    Returns
+    -------
+    success : bool
+        Flag indicating success or failure of the algorithm
+        
+    groups: iterable
+        A list of lists containing all groups of n_elements members fulfilling 
+        the connectivity constraints that maximize the sum of weights of all 
+        groups being used.        
+        
+    weights_total_sum : float
+        The total sum of all weights of the output groups
+    
+    """
+
+    assert G.number_of_nodes() % np.float(n_elements) == 0, "Number of sessions must be " + \
+                                "an integer multiple of n_elements"
+
+
+    ## initialize results object
+    if results is None:
+        results = Results(n_elements)
+
+    if n_unused is None:
+        n_unused = G.number_of_nodes()
+
+
+    ## base case
+    if n_unused == 0:
+        results.success = True
+        return results
+
+    ## recursion
+    else:
+        ## find all cliques in the graph G
+        cliques = list(nx.enumerate_all_cliques(G))
+
+        ## find all cliques that have the required number of elements
+        cliques = np.array([c for c in cliques if len(c)==n_elements])
+
+        ## sort cliques by weights
+        cliques, summed_weights = _sort_cliques_by_weights(G, cliques, n_elements)
+
+        ## find the total number of cliques with n_elements members
+        ncliques = len(cliques)
+
+        ## loop over all cliques:
+        for g,(cl,ww) in enumerate(zip(cliques, summed_weights)):
+            cl_topics = [G.node[c] for c in cl]
+
+            ## add the new clique to the list of output groups
+            results.update_groups(zip(cl, cl_topics))
+
+            ## add total weight of the clique:
+            results.update_weights(ww)
+
+            ## make a new deep copy for the next recursion step
+            G_new = copy.deepcopy(G)
+
+            ## remove clique from graph
+            for n in cl:
+                G_new.remove_node(n)
+
+            ## compute new unused number of nodes
+            n_unused = G_new.number_of_nodes()
+
+            ## if no unused nodes are left, return the selected groups,
+            ## otherwise recurse
+            results = find_solution(G_new, n_elements, n_unused, results)
+            if results is not None:
+                if results.success:
+                        return results
+
+            ## backtrack
+            else:
+                results.success = False
+                results.groups.pop(-1)
+                results.all_weights.pop(-1)
+                continue
+
+    if len(results.groups) == 0:
+        print("No solution found!")
+        results.success = False
+        return results
+
+    else:
+        results.groups.pop(-1)
+        results.all_weights.pop(-1)
+
+        results.success = False
+        return results
+
+
+
+
 
 
 
